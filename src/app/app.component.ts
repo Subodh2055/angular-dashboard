135968216e1e182ff684ea3e430ddbf351a78d8b
Subodh Bhandari <subodh.bhandari@sbsolutionsnepal.com>
2025-09-12 16:43:58 +0545

    finalizing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {JsonPipe} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLinkActive, RouterLink],
+  imports: [RouterOutlet, RouterLinkActive, RouterLink, JsonPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'angular-dashboard';
-  protected readonly version = 0;
+  title = `User's Dashboard`;
+  version = 0;
   date: Date = new Date();
 }
